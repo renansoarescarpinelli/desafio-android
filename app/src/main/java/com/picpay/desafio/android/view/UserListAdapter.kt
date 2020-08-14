c3f58a905536ec2a5e2a6df1521406108578e0d0
Renan Carpinelli <rcarpinelli@uolinc.com>
2020-08-14 08:40:11 -0300

    add data, model and view packages  - remove UserListItemViewHolder class and add into UserListAdapter

---
+++ app/src/main/java/com/picpay/desafio/android/view/UserListAdapter.kt
@@ -0,0 +1,61 @@
+package com.picpay.desafio.android.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.picpay.desafio.android.R
+import com.picpay.desafio.android.model.User
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.list_item_user.view.*
+
+class UserListAdapter : RecyclerView.Adapter<UserListAdapter.ViewHolder>() {
+
+    var users = emptyList<User>()
+        set(value) {
+            val result = DiffUtil.calculateDiff(
+                UserListDiffCallback(
+                    field,
+                    value
+                )
+            )
+            result.dispatchUpdatesTo(this)
+            field = value
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_user, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(users[position])
+    }
+
+    override fun getItemCount(): Int = users.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(user: User) {
+            itemView.name.text = user.name
+            itemView.username.text = user.username
+            itemView.progressBar.visibility = View.VISIBLE
+            Picasso.get()
+                .load(user.img)
+                .error(R.drawable.ic_round_account_circle)
+                .into(itemView.picture, object : Callback {
+                    override fun onSuccess() {
+                        itemView.progressBar.visibility = View.GONE
+                    }
+
+                    override fun onError(e: Exception?) {
+                        itemView.progressBar.visibility = View.GONE
+                    }
+                })
+        }
+    }
+}
